c2f8da902c9130ab595ef64793b25cbd9c28174a
paulo <paulo.inside@gmail.com>
2019-07-05 20:20:12 -0300

    Ready for presentation

--- sender/go/request-sender.go
+++ sender/go/request-sender.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -47,20 +49,38 @@ func ReadPropertiesFile(filename string) (AppConfigProperties, error) {
 	return config, nil
 }
 
-func main() {
-	props, err := ReadPropertiesFile("properties/k8s_example.properties")
-	service := os.Getenv("service")
+func PrintFile(filename string, msg string, err string) {
+	f, err1 := os.OpenFile("storage/"+filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err1 != nil {
+		panic(err1)
+	}
 
-	if err != nil && props["text"] != "" && service != "" {
+	defer f.Close()
+
+	if _, err1 = f.WriteString(msg + " | " + err + "\n"); err1 != nil {
+		panic(err1)
+	}
+}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	log_filename := strconv.Itoa(rand.Int())
+	props, err := ReadPropertiesFile("properties/k8s-example.properties")
+	text := os.Getenv("text")
+
+	if err != nil && props["service"] != "" && text != "" {
 		fmt.Println("Error while reading properties file")
 		return
 	}
 
-	url := "http://" + service + "/?text=" + props["text"]
+	url := "http://" + props["service"] + "/?text=" + text
 	for {
 		resp, err := http.Get(url)
 		fmt.Println(resp)
 		fmt.Println(err)
+		if err == nil {
+			PrintFile(log_filename, resp.Status, "")
+		}
 		time.Sleep(5 * time.Second)
 	}
 }
